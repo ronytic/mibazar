f8bd6fcb01baf15c89cb7d3b28a10928d4e4d968
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2022-09-17 14:47:07 -0400

    convirtiendo a matriz el formulario

--- vista/venta/fila.php
+++ vista/venta/fila.php
@@ -1,6 +1,6 @@
 <tr>
     <td>
-        <select name="id_producto" data-fila="<?= $numerofila ?>" class="form-control producto">
+        <select name="producto[<?= $numerofila ?>][id_producto]" data-fila="<?= $numerofila ?>" class="form-control producto">
             <option value="">Seleccione</option>
             <?php foreach ($productos as $producto) { ?>
                 <option value="<?= $producto['id_producto'] ?>">
@@ -12,15 +12,15 @@
         <img src="" alt="" class="imagen" height="100" data-fila="<?= $numerofila ?>">
     </td>
     <td>
-        <input type="number" name="stock" class="form-control stock" readonly data-fila="<?= $numerofila ?>">
+        <input type="number" name="producto[<?= $numerofila ?>][stock]" class="form-control stock" readonly data-fila="<?= $numerofila ?>">
     </td>
     <td>
-        <input type="number" name="precio" class="form-control precio" readonly data-fila="<?= $numerofila ?>">
+        <input type="number" name="producto[<?= $numerofila ?>][precio]" class="form-control precio" readonly data-fila="<?= $numerofila ?>">
     </td>
     <td>
-        <input type="number" name="cantidad" class="form-control cantidad" data-fila="<?= $numerofila ?>">
+        <input type="number" name="producto[<?= $numerofila ?>][cantidad]" class="form-control cantidad" data-fila="<?= $numerofila ?>">
     </td>
     <td>
-        <input type="number" name="total" class="form-control total" readonly data-fila="<?= $numerofila ?>">
+        <input type="number" name="producto[<?= $numerofila ?>][total]" class="form-control total" readonly data-fila="<?= $numerofila ?>">
     </td>
 </tr>
